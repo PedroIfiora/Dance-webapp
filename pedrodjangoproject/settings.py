72b661fb72c5894a51b8376b0562fb73b6c38812
Pedroifiora <pedroifiora@gmail.com>
2022-08-31 13:02:39 +0100

    added a env file

--- pedrodjangoproject/settings.py
+++ pedrodjangoproject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t%vf#*dacfwj1bj0!&p88f8au+r=gmk&+0*u!!289ohx7dn1fs'
+
+#Now instead of exposing our secret keys and OAuth keys in the settings,
+#let's retrieve them through their key names as follows.
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
